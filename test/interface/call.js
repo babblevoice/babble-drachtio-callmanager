35c01d6c6f98afd742ce79e198c75780bb54f9e2
nick <nick@babblevoice.com>
2021-11-01 14:51:21 +0000

    test a simple call + rework

--- test/interface/call.js
+++ test/interface/call.js
@@ -3,8 +3,20 @@ const expect = require( "chai" ).expect
 const callmanager = require( "../../index.js" )
 const call = require( "../../lib/call.js" )
 const srf = require( "../mock/srf.js" )
+const projectrtp = require( "projectrtp" ).projectrtp
 
+/* These DO NOT form part of our interface */
 const clearcallmanager = require( "../../lib/callmanager.js" )._clear
+const callstore = require( "../../lib/store.js" )
+
+
+after( async () => {
+  await projectrtp.shutdown()
+} )
+
+before( async () => {
+  await projectrtp.run()
+} )
 
 /* All call objects should be created by the framework - but we create them to test */
 
@@ -116,6 +128,99 @@ describe( "call object", function() {
     /* if uac */
     expect( c ).to.not.have.property( "source" ).that.is.a( "object" )
     expect( c ).to.not.have.property( "sip" ).that.is.a( "object" )
+
+  } )
+
+
+  it( `uas.newuac`, async function() {
+
+    let clienttestsdp = `v=0
+o=- 1608235282228 0 IN IP4 127.0.0.1
+s=
+c=IN IP4 192.168.0.141
+t=0 0
+m=audio 20000 RTP/AVP 8 101
+a=rtpmap:101 telephone-event/8000
+a=fmtp:101 0-16
+a=sendrecv`.replace(/(\r\n|\n|\r)/gm, "\r\n")
+
+    let servertestsdp = `v=0
+o=- 1608235282228 0 IN IP4 127.0.0.1
+s=
+c=IN IP4 192.168.0.200
+t=0 0
+m=audio 18000 RTP/AVP 8 101
+a=rtpmap:101 telephone-event/8000
+a=fmtp:101 0-16
+a=sendrecv`.replace(/(\r\n|\n|\r)/gm, "\r\n")
+
+    /* We need to create a callmanager to create a call object */
+    let options = {
+      "srf": {
+        "use": ( method, asynccb ) => {
+          expect( method ).to.equal( "invite" )
+        },
+        "createUAC": async ( contact, options, callbacks ) => {
+
+          expect( callbacks ).to.have.property( "cbRequest" ).that.is.a( "function" )
+          expect( callbacks ).to.have.property( "cbProvisional" ).that.is.a( "function" )
+
+          let dialog = {
+            "sip": {
+              "localTag": "lkwefwiuwie",
+              "remoteTag": "wkenckw3w3"
+            },
+            "remote": {
+              "sdp": clienttestsdp
+            },
+            "destroy": () => {}
+          }
+          return dialog
+        },
+
+        "createUAS": async( ) => {
+          let dialog = {
+            "sip": {
+              "localTag": "87dh3qhd82hd",
+              "remoteTag": "dfskjfwf3f"
+            },
+            "remote": {
+              "sdp": servertestsdp
+            },
+            "on": () => {},
+            "destroy": () => {}
+          }
+          return dialog
+        }
+      }
+    }
+    await callmanager.callmanager( options )
+
+    let req = new srf.req()
+    req.setparsedheader( "call-id", "1234" )
+    req.setparsedheader( "from", {
+      "params": {
+        "tag": "from-tag"
+      }
+    } )
+
+    let c = new call.call( req, {} )
+
+    let child = await c.newuac( "1000@dummy" )
+
+    expect( await callstore.stats() ).to.deep.include( {
+      "storebycallid": 2,
+      "storebyuuid": 2,
+      "storebyentity": 0
+    } )
+
+    await child.hangup()
+
+    expect( await callstore.stats() ).to.deep.include( {
+      "storebycallid": 0,
+      "storebyuuid": 0,
+      "storebyentity": 0
+    } )
 
   } )
 } )
--- test/mock/srf.js
+++ test/mock/srf.js
@@ -16,6 +16,24 @@ class req {
     }
 
     this.events = {}
+
+    this.msg = {
+      "body": `v=0
+o=Z 1610744131900 1 IN IP4 127.0.0.1
+s=Z
+c=IN IP4 192.168.0.200
+t=0 0
+m=audio 18540 RTP/AVP 106 9 98 101 0 8 18 3
+a=rtpmap:106 opus/48000/2
+a=fmtp:106 maxplaybackrate=16000; sprop-maxcapturerate=16000; minptime=20; cbr=1; maxaveragebitrate=20000; useinbandfec=1
+a=rtpmap:98 telephone-event/48000
+a=fmtp:98 0-16
+a=rtpmap:101 telephone-event/8000
+a=fmtp:101 0-16
+a=rtpmap:18 G729/8000
+a=fmtp:18 annexb=no
+a=sendrecv`.replace(/(\r\n|\n|\r)/gm, "\r\n")
+    }
   }
 
   getParsedHeader( header ) {
