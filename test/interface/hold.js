0409fabb4414f5968a490a8b80ddc9f11265a51d
nick <nick@babblevoice.com>
2021-11-09 11:29:08 +0000

    add another test case

--- test/interface/hold.js
+++ test/interface/hold.js
@@ -124,4 +124,51 @@ a=sendrecv`.replace(/(\r\n|\n|\r)/gm, "\r\n")
     expect( msgsent.headers.Subject ).to.equal( "Call off hold" )
     expect( msgsent.body ).to.include( "a=sendrecv" )
   } )
+
+
+  it( `place call on hold - 0.0.0.0`, async function() {
+    let srfscenario = new srf.srfscenario()
+
+    let call = await new Promise( ( resolve ) => {
+      srfscenario.oncall( async ( call ) => { resolve( call ) } )
+      srfscenario.inbound()
+    } )
+
+    let eventreceived = false
+    call.on( "call.hold", ( anotherreftocall ) => {
+      eventreceived = true
+    } )
+
+    await call.answer()
+
+    let req = new srf.req( new srf.options() )
+    let res = new srf.res()
+
+    let sipcodesent, msgsent
+    res.onsend( ( sipcode, msg ) => {
+      sipcodesent = sipcode
+      msgsent = msg
+    } )
+
+    req.msg.body = `v=0
+o=- 1608235282228 0 IN IP4 127.0.0.1
+s=
+c=IN IP4 0.0.0.0
+t=0 0
+m=audio 20000 RTP/AVP 97 101
+a=rtpmap:97 ilbc/8000
+a=rtpmap:101 telephone-event/8000
+a=fmtp:101 0-16
+a=fmtp:97 mode=20
+a=sendrecv`.replace(/(\r\n|\n|\r)/gm, "\r\n")
+
+    call._dialog.callbacks.modify( req, res )
+
+    await call.hangup()
+
+    expect( eventreceived ).to.be.true
+    expect( sipcodesent ).to.equal( 200 )
+    expect( msgsent.headers.Subject ).to.equal( "Call on hold" )
+    expect( msgsent.body ).to.include( "a=inactive" )
+  } )
 } )
