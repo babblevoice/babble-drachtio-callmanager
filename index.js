f8fa0c05165e6d69c11f8eba53790b1ea28b07cd
nick <nick@babblevoice.com>
2021-12-06 16:15:00 +0000

    we need to be able to add a registrar

--- index.js
+++ index.js
@@ -9,7 +9,8 @@ const default_options = {
   "uactimeout": 30000, /* timeout when calling a client */
   "seexpire": 120000, /* session expires timeout */
   "rfc2833": true,  /* Enable RFC 2833 - DTMF */
-  "late": false  /* Late negotiation */
+  "late": false,  /* Late negotiation */
+  "registrar": false /* our registrar object or falsey */
 }
 
 module.exports.callmanager = async( options ) => {
