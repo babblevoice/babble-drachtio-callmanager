c0dfcbf9e5f411c154016ce9024de9aa2dd8fc73
nick <nick@babblevoice.com>
2021-12-08 14:11:29 +0000

    style

--- index.js
+++ index.js
@@ -14,8 +14,7 @@ const default_options = {
 }
 
 module.exports.callmanager = async( options ) => {
-  Object.assign( default_options, options )
-  return callmanager.callmanager( default_options )
+  return callmanager.callmanager( { ...default_options, ...options } )
 }
 
 module.exports.hangupcodes = callmanager.hangupcodes
