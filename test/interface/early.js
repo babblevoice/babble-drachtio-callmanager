b43ddf99a6a5e1bf9816cd0070b5f4a7b968de07
nick <nick@babblevoice.com>
2022-09-05 20:35:09 +0100

    early bugs

--- test/interface/early.js
+++ test/interface/early.js
@@ -23,8 +23,17 @@ describe( "call early", function() {
 
 
   it( `Create call and send 183 - early`, async () => {
-    let srfscenario = new srf.srfscenario()
 
+    /*
+    Phone                 BV                   Gateway
+    |---------INVITE------>|                      |(1)
+    |                      |---------INVITE------>|(2)
+    |                      |<--------183 (w-sdp)--|(3)
+    |<--------183 (w-sdp)--|                      |(4)
+    */
+
+    /* Setup the mock RTP server */ 
+    let srfscenario = new srf.srfscenario()
     let rtpserver = callmanager.projectrtp.proxy.listen()
 
     let connection = net.createConnection( 9002, "127.0.0.1" )
@@ -37,18 +46,49 @@ describe( "call early", function() {
       connection.write( projectrtpmessage.createmessage( {"status":{"channel":{"available":5000,"current":0},"workercount":12,"instance":"ca0ef6a9-9174-444d-bdeb-4c9eb54d4566"}} ) )
     } )
 
-    let msgid
     let mixing
     let messagestate = projectrtpmessage.newstate()
+    let channelmessages = []
+    let opencount = 0
+    
     connection.on( "data", ( data ) => {
       projectrtpmessage.parsemessage( messagestate, data, ( msg ) => {
-        if( "open" === msg.channel ) {
-          msgid = msg.id
-          setTimeout( () => connection.write( projectrtpmessage.createmessage( {"local":{"port":10008,"dtls":{"fingerprint":"Some fingerprint","enabled":false},"address":"192.168.0.141"},"id": msg.id, "uuid": uuidv4(),"action":"open","status":{"channel":{"available":4995,"current":5},"workercount":12,"instance":"ca0ef6a9-9174-444d-bdeb-4c9eb54d4566"}} ) ), 2 )
-        } else if ( "close" === msg.channel ) {
-          connection.write( projectrtpmessage.createmessage( {"id": msg.id,"uuid":msg.uuid,"action":"close","reason":"requested","stats":{"in":{"mos":4.5,"count":586,"dropped":0,"skip":0},"out":{"count":303,"skip":0},"tick":{"meanus":124,"maxus":508,"count":597}}} ) )
-        } else if ( "mix" === msg.channel ) {
-          mixing = true
+        try{
+          channelmessages.push( msg )
+          if( "open" === msg.channel ) {
+            if( 0 == opencount ) {
+              setTimeout( () => 
+                connection.write( 
+                  projectrtpmessage.createmessage( 
+                    {"local":{"port":10008,"dtls":
+                      {"fingerprint":"Some fingerprint","enabled":false},
+                      "address":"192.168.0.141"},
+                      "id": msg.id, 
+                      "uuid": uuidv4(),
+                      "action":"open",
+                      "status":{"channel":{"available":4995,"current":5},"workercount":12,"instance":"ca0ef6a9-9174-444d-bdeb-4c9eb54d4566"}
+                    } ) ), 2 )
+            } else {
+              setTimeout( () => 
+                connection.write( 
+                  projectrtpmessage.createmessage( 
+                    {"local":{"port": 10010,"dtls":
+                      {"fingerprint":"Some fingerprint","enabled":false},
+                      "address":"192.168.0.141"},
+                      "id": msg.id, 
+                      "uuid": uuidv4(),
+                      "action":"open",
+                      "status":{"channel":{"available":4995,"current":5},"workercount":12,"instance":"ca0ef6a9-9174-444d-bdeb-4c9eb54d4566"}
+                    } ) ), 2 )
+            }
+            opencount++
+          } else if ( "close" === msg.channel ) {
+            connection.write( projectrtpmessage.createmessage( {"id": msg.id,"uuid":msg.uuid,"action":"close","reason":"requested","stats":{"in":{"mos":4.5,"count":586,"dropped":0,"skip":0},"out":{"count":303,"skip":0},"tick":{"meanus":124,"maxus":508,"count":597}}} ) )
+          } else if ( "mix" === msg.channel ) {
+            mixing = true
+          }
+        } catch( e ) {
+          console.error( e )
         }
       } )
     } )
@@ -57,6 +97,8 @@ describe( "call early", function() {
     await new Promise( ( r ) => setTimeout( () => r(), 100 ) )
 
     srfscenario.oncreateUAC( async ( contact, options, callbacks ) => {
+
+      /* Step 3. This is the mocked gateway message back to our newcall. */
       callbacks.cbProvisional( {
         "status": 183,
         "msg": {
@@ -76,16 +118,21 @@ a=sendrecv`.replace(/(\r\n|\n|\r)/gm, "\r\n")
       throw { "status": 503 }
     } )
 
-    let msgsent
+    /* Step 1. Phone sends INVITE */
     let call = await new Promise( ( resolve ) => {
       srfscenario.oncall( async ( call ) => { resolve( call ) } )
       srfscenario.inbound()
-      srfscenario.res.onsend( (c) => {
-        msgsent = c
-      })
+    } )
+
+    /* Step 4. BV sends 183 back to phone */
+    let msgsent, msginfo
+    srfscenario.res.onsend( ( c, i ) => {
+      msgsent = c
+      msginfo = i
     } )
 
 
+    /* Step 2. New INVITE to the remote Gateway */
     let newcall = await call.newuac( { "contact": "callto" } )
     
     await call._onhangup( "wire" )
@@ -94,6 +141,17 @@ a=sendrecv`.replace(/(\r\n|\n|\r)/gm, "\r\n")
     expect( call.state.early ).to.be.true
     expect( mixing ).to.be.true
     expect( msgsent ).to.equal( 183 )
+
+    expect( channelmessages[ 0 ].channel ).to.equal( "open" )
+    expect( channelmessages[ 1 ].channel ).to.equal( "remote" )
+    expect( channelmessages[ 1 ].remote.port ).to.equal( 20000 )
+    expect( channelmessages[ 2 ].channel ).to.equal( "open" )
+    expect( channelmessages[ 2 ].remote.port ).to.equal( 18540 )
+    expect( channelmessages[ 3 ].channel ).to.equal( "mix" )
+    expect( channelmessages[ 4 ].channel ).to.equal( "close" )
+    expect( channelmessages[ 5 ].channel ).to.equal( "close" )
+
+    expect( msginfo.body ).to.include( "audio 10010 RTP/AVP" )
 
     connection.destroy()
     rtpserver.destroy()
