9f28a1ddd3d5a49096e08a4b3a5d772c56d28b93
Nick Knight <nick@babblevoice.com>
2023-08-23 16:28:16 +0100

    hangup call

--- test/unit/call.js
+++ test/unit/call.js
@@ -1,18 +1,18 @@
+
 const expect = require( "chai" ).expect
-const callstore = require( "../../lib/call.js" )
 const srf = require( "../mock/srf.js" )
 
 describe( "call.js", function() {
 
-  it( `set remote name and id`, async function() {
-    let srfscenario = new srf.srfscenario()
+  it( "set remote name and id", async function() {
+    const srfscenario = new srf.srfscenario()
 
     let newcallcalled = false
     srfscenario.options.em.on( "call.new", ( /*newcall*/ ) => {
       newcallcalled = true
     } )
 
-    let call = await new Promise( ( resolve ) => {
+    const call = await new Promise( ( resolve ) => {
       srfscenario.oncall( async ( call ) => { resolve( call ) } )
       srfscenario.inbound()
     } )
@@ -24,6 +24,9 @@ describe( "call.js", function() {
     // the names mismatch and can be confusing, we might consider some polishing here?
     expect(call.remote.name).equals("foo")
     expect(call.remote.user).equals("123456789")
+    expect( newcallcalled ).to.be.true
+
+    await call.hangup()
 } )
 
 } )
