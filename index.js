64a2d258fed4eb74db79c9fbc55be5cc69bd4c0b
Nick Knight <nick@babblevoice.com>
2025-05-08 15:24:23 +0100

    reduce to recomended standards - inc default uac timeout

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const projectrtp = require( "@babblevoice/projectrtp" ).projectrtp
 const default_options = {
   "preferedcodecs": "g722 ilbc pcmu pcma",
   //"transcode": true, - this never made it into the software - TODO
-  "uactimeout": 30000, /* timeout when calling a client */
+  "uactimeout": 32000, /* timeout when calling a client: Default: 64 × T1 = 32 seconds */
   "seexpire": 1800*1000, /* session expires timeout mS recomended in RFC 4028 - 30 minutes */
   "rfc2833": true,  /* Enable RFC 2833 - DTMF */
   "late": false,  /* Late negotiation */
